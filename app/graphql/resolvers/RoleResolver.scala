038eb326ce66fa7f78d659f81e4dc2669b4a611f
Dede Andres <dede.andres@dana.id>
2019-07-03 17:56:47 +0700

    Update RoleResolver.scala

--- app/graphql/resolvers/RoleResolver.scala
+++ app/graphql/resolvers/RoleResolver.scala
@@ -10,7 +10,9 @@ import scala.concurrent.ExecutionContext
 
 class RoleResolver @Inject()(roleRepository: RoleRepository, implicit val executionContext: ExecutionContext){
 
+  import java.util.UUID
+
   def roles: Future[List[Role]] = roleRepository.findAll()
 
-  def findRole(id: Long): Future[List[Role]] = roleRepository.findById(id)
+  def findRole(id: UUID): Future[List[Role]] = roleRepository.findById(id)
 }
