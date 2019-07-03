e0e6fc10ca09e8ae0e21b6b03916c17a3b45960b
Dede Andres <dede.andres@dana.id>
2019-07-03 17:56:32 +0700

    Update UserRepository.scala

--- app/repositories/UserRepository.scala
+++ app/repositories/UserRepository.scala
@@ -6,13 +6,15 @@ import scala.concurrent.Future
 
 trait UserRepository {
 
+  import java.util.UUID
+
   def findAll(): Future[List[User]]
 
-  def find(id: Long): Future[Option[User]]
+  def find(id: UUID): Future[Option[User]]
 
   def create(user: User): Future[User]
 
-  def delete(id: Long): Future[Boolean]
+  def delete(id: UUID): Future[Boolean]
 
   def update(user: User): Future[User]
 
