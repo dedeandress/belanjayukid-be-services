373003e48686524c4b3ff2c386ca939cd5c7f8b3
Dede Andres <dede.andres@dana.id>
2019-07-03 17:56:53 +0700

    Update UserResolver.scala

--- app/graphql/resolvers/UserResolver.scala
+++ app/graphql/resolvers/UserResolver.scala
@@ -7,18 +7,20 @@ import scala.concurrent.ExecutionContext
 
 class UserResolver @Inject()(userRepository: UserRepository, implicit val executionContext: ExecutionContext){
 
+  import java.util.UUID
+
   import models.User
 
   import scala.concurrent.Future
 
   def users: Future[List[User]] = userRepository.findAll()
 
-  def addUser(name: String, email: String, roleId: Long): Future[User] = userRepository.create(User(name= name, email = email, roleId = roleId))
+  def addUser(username: String, password: String ,email: String, userProfileId: UUID): Future[User] = userRepository.create(User(username= username, password = password,email = email, userProfileId = userProfileId))
 
-  def deleteUser(id: Long): Future[Boolean] = userRepository.delete(id)
+  def deleteUser(id: UUID): Future[Boolean] = userRepository.delete(id)
 
-  def updateUser(id: Long, name: String, email: String, roleId: Long): Future[User] = userRepository.update(User(Some(id), name, email, roleId))
+  def updateUser(id: UUID, username: String, password: String, email: String,userProfileId: UUID): Future[User] = userRepository.update(User(id, username, password, email, userProfileId))
 
-  def findUser(id: Long): Future[Option[User]] = userRepository.find(id)
+  def findUser(id: UUID): Future[Option[User]] = userRepository.find(id)
 
 }
