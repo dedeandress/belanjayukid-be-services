046b42572bfa01afa1fc01727b1d5d01c6f2ebc0
Dede Andres <dede.andres@dana.id>
2019-07-03 17:57:00 +0700

    Update Role.scala

--- app/models/Role.scala
+++ app/models/Role.scala
@@ -1,21 +1,34 @@
 package models
 
-import slick.jdbc.H2Profile.api.{Table => SlickTable, _}
+import java.util.UUID
+import java.util.UUID.randomUUID
+
+import slick.jdbc.PostgresProfile.api.{Table => SlickTable, _}
 import slick.lifted.{Tag => SlickTag}
 import spray.json.{DefaultJsonProtocol, JsonFormat}
+import spray.json.{JsString, JsValue}
+import spray.json.deserializationError
+import spray.json.RootJsonFormat
 
+case class Role(id: UUID = randomUUID, name: String)
 
-case class Role(id: Option[Long] = None, name: String, description: String)
-
-object Role extends ((Option[Long], String, String)=>Role){
+object Role extends ((UUID, String)=>Role){
   class RoleTable(slickTag: SlickTag) extends SlickTable[Role](slickTag, "roles"){
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def id = column[UUID]("id", O.PrimaryKey)
     def name = column[String]("name")
-    def description = column[String]("description")
-    def * = (id.?, name, description).mapTo[Role]
+    def * = (id, name).mapTo[Role]
   }
 }
 
 object RoleJsonProtocol extends DefaultJsonProtocol{
-  implicit val roleJsonProtocol: JsonFormat[Role] = jsonFormat3(Role)
+
+  implicit object UuidJsonFormat extends RootJsonFormat[UUID] {
+    def write(x: UUID) = JsString(x.toString) //Never execute this line
+    def read(value: JsValue) = value match {
+      case JsString(x) => UUID.fromString(x)
+      case x           => deserializationError("Expected UUID as JsString, but got " + x)
+    }
+  }
+
+  implicit val roleJsonProtocol: JsonFormat[Role] = jsonFormat2(Role)
 }
