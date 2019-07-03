c2de7d1df63ce1091fddf9de5731944fe1b7d224
Dede Andres <dede.andres@dana.id>
2019-07-03 17:57:13 +0700

    Update RoleRepository.scala

--- app/repositories/RoleRepository.scala
+++ app/repositories/RoleRepository.scala
@@ -2,11 +2,13 @@ package repositories
 
 trait RoleRepository {
 
+  import java.util.UUID
+
   import models.Role
 
   import scala.concurrent.Future
 
   def findAll(): Future[List[Role]]
 
-  def findById(id: Long): Future[List[Role]]
+  def findById(id: UUID): Future[List[Role]]
 }
