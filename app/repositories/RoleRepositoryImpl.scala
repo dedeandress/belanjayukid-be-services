48707069ea0aa569b753de33846700955ac251b9
Dede Andres <dede.andres@dana.id>
2019-07-03 17:57:15 +0700

    Update RoleRepositoryImpl.scala

--- app/repositories/RoleRepositoryImpl.scala
+++ app/repositories/RoleRepositoryImpl.scala
@@ -3,12 +3,13 @@ package repositories
 import com.google.inject.{Inject, Singleton}
 import modules.AppDatabase
 import models.Role
+import java.util.UUID
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class RoleRepositoryImpl @Inject()(database: AppDatabase, implicit val executionContext: ExecutionContext) extends RoleRepository {
+class RoleRepositoryImpl @Inject()(database: AppDatabase, implicit val executionContext: ExecutionContext) extends RoleRepository{
 
   val db = database.db
   val profile = database.profile
@@ -20,15 +21,16 @@ class RoleRepositoryImpl @Inject()(database: AppDatabase, implicit val execution
 
   override def findAll(): Future[List[Role]] = db.run(Actions.findAll)
 
-  override def findById(id: Long): Future[List[Role]] = db.run(Actions.findById(id))
+  override def findById(id: UUID): Future[List[Role]] = db.run(Actions.findById(id))
 
   object Actions{
+
 
     def findAll: DBIO[List[Role]] = for{
       roles <- roleQuery.result
     }yield roles.toList
 
-    def findById(id: Long): DBIO[List[Role]] = for{
+    def findById(id: UUID): DBIO[List[Role]] = for{
       roles <- roleQuery.filter(_.id===id).result
     }yield roles.toList
   }
