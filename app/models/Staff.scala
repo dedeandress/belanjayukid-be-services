e1a2e0fbf8cc619a79bf565414aca752ada0da73
Dede Andres <dede.andres@dana.id>
2019-07-07 00:04:31 +0700

    Update Staff.scala

--- app/models/Staff.scala
+++ app/models/Staff.scala
@@ -2,6 +2,8 @@ package models
 
 import java.util.UUID
 
+import models.Role.RoleTable
+import models.User.UserTable
 import slick.jdbc.PostgresProfile.api.{Table => SlickTable, _}
 import slick.lifted.{Tag => SlickTag}
 import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat, RootJsonFormat, deserializationError}
@@ -9,12 +11,19 @@ import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat, RootJsonF
 case class Staff(id: UUID, userId: UUID, roleId: UUID)
 
 object Staff extends ((UUID, UUID, UUID)=>Staff) {
+
+  val users = TableQuery[UserTable]
+  val roles = TableQuery[RoleTable]
+
   class StaffTable(slickTag: SlickTag) extends SlickTable[Staff](slickTag,"staff"){
     def id = column[UUID]("id")
     def userId = column[UUID]("user_id")
     def roleId = column[UUID]("role_id")
+    def userIdFK = foreignKey("user_id", userId, users)(_.id)
+    def roleIdFK = foreignKey("role_id", roleId, roles)(_.id)
     def * = (id, userId, roleId).mapTo[Staff]
   }
+
 }
 
 object StaffJsonProtocol extends DefaultJsonProtocol {
