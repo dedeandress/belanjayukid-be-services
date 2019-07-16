4c8307b279dedb1962b0e4b7326c01f7676a5080
Dede Andres <dede.andres@dana.id>
2019-07-16 22:29:58 +0700

    Update UserResolver.scala

--- app/graphql/resolvers/UserResolver.scala
+++ app/graphql/resolvers/UserResolver.scala
@@ -1,26 +1,38 @@
 package graphql.resolvers
 
 import com.google.inject.Inject
-import repositories.UserRepository
+import services.UserService
 
 import scala.concurrent.ExecutionContext
 
-class UserResolver @Inject()(userRepository: UserRepository, implicit val executionContext: ExecutionContext){
+class UserResolver @Inject()(implicit val executionContext: ExecutionContext, userService: UserService){
 
   import java.util.UUID
 
+  import graphql.input.UserInput
   import models.User
+  import play.api.mvc.Headers
 
   import scala.concurrent.Future
 
-  def users: Future[List[User]] = userRepository.findAll()
+  def users: Future[List[User]] = userService.users
 
-  def addUser(user: User): Future[User] = userRepository.create(user)
+  def addUser(user: User): Future[User] = userService.addUser(user)
 
-  def deleteUser(id: UUID): Future[Boolean] = userRepository.delete(id)
+  def deleteUser(id: UUID): Future[Boolean] = userService.deleteUser(id)
 
-  def updateUser(id: UUID, username: String, password: String, email: String,userProfileId: UUID): Future[User] = userRepository.update(User(id, username, password, email, userProfileId))
+  def updateUser(id: UUID, username: String, password: String, email: String): Future[User] = userService.updateUser(id,username, password, email)
 
-  def findUser(id: UUID): Future[Option[User]] = userRepository.find(id)
+  def findUser(id: UUID): Future[Option[User]] = userService.findUser(id)
+
+  def login(username: String, password: String): Future[String] = userService.login(username, password)
+
+  def inputUser(user: UserInput): Future[User] ={
+    userService.addUser(user)
+  }
+
+  def getUser(headers: Headers): Future[Option[User]] = {
+    userService.isAuthorized(headers)
+  }
 
 }
