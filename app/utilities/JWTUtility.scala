a4c50fd32e8417652f1b3e6212a55651a2a32886
Dede Andres <dede.andres@dana.id>
2019-07-16 22:30:32 +0700

    Create JWTUtility.scala

---
+++ app/utilities/JWTUtility.scala
@@ -0,0 +1,52 @@
+package utilities
+
+import java.time.{ZoneId, ZonedDateTime}
+import java.util.Date
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.google.inject.Inject
+import models.User
+import repositories.UserRepository
+
+class JWTUtility @Inject()(userRepository: UserRepository){
+
+  import play.api.mvc.Headers
+
+  def getUser(headers: Headers) = {
+    import errors.Unauthorized
+    if(headers.get("Authorization").isDefined) {
+      import java.util.UUID
+      val token = JWTUtility.extractToken(headers.get("Authorization").get)
+      val decodeJWT = JWTUtility.validateJWT(token.get)
+      val id = decodeJWT.getClaim("id").asString()
+      userRepository.find(UUID.fromString(id))
+    }else throw new Unauthorized("Unauthorized")
+  }
+}
+
+object JWTUtility {
+
+  import com.auth0.jwt.interfaces.DecodedJWT
+
+  def generateJWT(user: User) = {
+    val algorithm: Algorithm = Algorithm.HMAC256(BelanjaYukConstant.secretKey)
+    JWT.create().withIssuer("BelanjaYuk.id")
+      .withClaim("id", user.id.toString)
+      .withExpiresAt(Date.from(ZonedDateTime.now(ZoneId.systemDefault()).plusMinutes(60).toInstant))
+      .sign(algorithm)
+  }
+
+  def validateJWT(jwt: String): DecodedJWT = {
+    val algorithm: Algorithm = Algorithm.HMAC256(BelanjaYukConstant.secretKey)
+    JWT.require(algorithm).withIssuer("BelanjaYuk.id")
+      .build().verify(jwt)
+  }
+
+  def extractToken(authHeader: String): Option[String] = {
+    authHeader.split("Bearer ") match {
+      case Array(_, token) => Some(token)
+      case _ => None
+    }
+  }
+}
