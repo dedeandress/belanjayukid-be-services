cc236a1a4a2e8f7bfb6183b8213d04dc27cfd915
Dede Andres <dede.andres@dana.id>
2019-07-16 22:30:22 +0700

    Update UserService.scala

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,15 +1,68 @@
 package services
 
-import com.google.inject.Inject
-import graphql.resolvers.UserResolver
 import java.util.UUID
 
+import com.google.inject.Inject
 import models.User
+import models.UserProfile
+import repositories.{UserProfileRepository, UserRepository}
 
-class UserService @Inject()(userResolver: UserResolver){
+import scala.concurrent.ExecutionContext
 
-  def registerUser(username: String, password: String ,email: String, userProfileId: UUID) ={
-    userResolver.addUser(new User(username = username, password = password, email = email, userProfileId = userProfileId))
+class UserService @Inject()(userProfileRepository: UserProfileRepository, userRepository: UserRepository, implicit val executionContext: ExecutionContext){
+
+  import graphql.input.UserInput
+
+  import scala.concurrent.Future
+
+//  def registerUser(username: String, password: String ,email: String, userProfileId: UUID) ={
+//    userResolver.addUser(new User(username = username, password = password, email = email))
+//  }
+//
+  def addUser(user: UserInput): Future[User] = {
+    userRepository.create(new User(username = user.username, password = user.password, email = user.email))
+  }
+  import play.api.mvc.Headers
+  import utilities.JWTUtility
+
+  def login(username: String, password: String): Future[String] = {
+    userRepository.findUser(username).map{
+      user=>
+        if(user.get.password==password)
+        JWTUtility.generateJWT(user.get)
+        else throw new Exception("not found")
+    }
+  }
+  import com.auth0.jwt.interfaces.DecodedJWT
+  import graphql.input.UserProfileInput
+
+  def isAuthorized(headers: Headers) = {
+    import errors.Unauthorized
+    if(headers.get("Authorization").isDefined) {
+      val token = JWTUtility.extractToken(headers.get("Authorization").get).get
+      val decodeJWT: DecodedJWT = JWTUtility.validateJWT(token)
+      val id = decodeJWT.getClaim("id").asString()
+      val user = userRepository.find(UUID.fromString(id))
+      user
+    }else throw new Unauthorized("you are not authorized")
+  }
+
+  def users: Future[List[User]] = userRepository.findAll()
+
+  def addUser(user: User): Future[User] = userRepository.create(user)
+
+  def deleteUser(id: UUID): Future[Boolean] = userRepository.delete(id)
+
+  def updateUser(id: UUID, username: String, password: String, email: String): Future[User] = userRepository.update(User(id, username, password, email))
+
+  def findUser(id: UUID): Future[Option[User]] = userRepository.find(id)
+
+  def findUserProfile(userId: UUID): Future[Option[UserProfile]] = userProfileRepository.findByUserId(userId)
+
+  def insertUserProfile(userProfileInput: UserProfileInput): Future[UserProfile] ={
+    userProfileRepository.addUserProfile(new UserProfile(fullName = userProfileInput.fullName, noNik = userProfileInput.noNik,
+      phoneNumber = userProfileInput.phoneNumber, address = userProfileInput.address, dateOfBirth = userProfileInput.dateOfBirth,
+      userId = userProfileInput.userId))
   }
 
 }
