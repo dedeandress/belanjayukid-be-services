2acbc057d4823f096c23084fcae04f9aba362474
Dede Andres <dede.andres@dana.id>
2019-07-24 16:13:26 +0700

    Create StaffRepository.scala

---
+++ app/repositories/repositoryInterfaces/StaffRepository.scala
@@ -0,0 +1,13 @@
+package repositories.repositoryInterfaces
+
+import java.util.UUID
+
+import models.Staff
+
+import scala.concurrent.Future
+
+trait StaffRepository {
+  def addStaff(staff: Staff): Future[Option[Staff]]
+
+  def findById(id: UUID): Future[Option[Staff]]
+}
