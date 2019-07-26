2edfae6fb443ff56002dbb161f835df80c913143
Dede Andres <dede.andres@dana.id>
2019-07-26 16:53:01 +0700

    Create ProductDetailRepository.scala

---
+++ app/repositories/repositoryInterfaces/ProductDetailRepository.scala
@@ -0,0 +1,16 @@
+package repositories.repositoryInterfaces
+
+import java.util.UUID
+
+import models.ProductDetail
+
+import scala.concurrent.Future
+
+trait ProductDetailRepository {
+
+  def findProductDetail(id: UUID): Future[Option[ProductDetail]]
+
+  def addProductDetail(productDetail: ProductDetail): Future[ProductDetail]
+
+  def findProductDetailByProductId(productId: UUID): Future[Seq[ProductDetail]]
+}
