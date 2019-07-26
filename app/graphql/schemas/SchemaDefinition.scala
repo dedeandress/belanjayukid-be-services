e4035d36f8a1a6f6413ad00ca52c7da4f7a50797
Dede Andres <dede.andres@dana.id>
2019-07-26 16:53:37 +0700

    Update SchemaDefinition.scala

--- app/graphql/schemas/SchemaDefinition.scala
+++ app/graphql/schemas/SchemaDefinition.scala
@@ -2,11 +2,14 @@ package graphql.schemas
 
 import com.google.inject.Inject
 import graphql.GraphQLType
-import graphql.resolvers.{RoleResolver, StaffResolver, UserProfileResolver, UserResolver}
+import graphql.resolvers.{CategoryResolver, ProductResolver, RoleResolver, StaffResolver, UserProfileResolver, UserResolver}
 import sangria.schema.{Field, OptionType}
 
 
-class SchemaDefinition @Inject()(staffResolver: StaffResolver, userResolver: UserResolver, userProfileResolver: UserProfileResolver, roleResolver: RoleResolver, graphQLType: GraphQLType){
+class SchemaDefinition @Inject()(staffResolver: StaffResolver
+                                 , userResolver: UserResolver, userProfileResolver: UserProfileResolver
+                                 , roleResolver: RoleResolver, categoryResolver: CategoryResolver
+                                 , productResolver: ProductResolver, graphQLType: GraphQLType){
 
 //  val Queries: List[Field[Unit, Unit]] = List(
 //    Field(
@@ -22,6 +25,12 @@ class SchemaDefinition @Inject()(staffResolver: StaffResolver, userResolver: Use
       fieldType = OptionType(graphQLType.StaffType),
       arguments = graphQLType.StaffInputArg :: Nil,
       resolve = sangriaContext => staffResolver.addStaff(sangriaContext.arg(graphQLType.StaffInputArg))
+    ),
+    Field(
+      name = "addProduct",
+      fieldType = graphQLType.ProductType,
+      arguments = graphQLType.ProductInputArg :: Nil,
+      resolve = sangriaContext => productResolver.addProduct(sangriaContext.arg(graphQLType.ProductInputArg))
     )
   )
 
