3abc8e5f5a01adbadb12ab6b45f17c5c2c2ae574
Dede Andres <dede.andres@dana.id>
2019-07-26 16:51:58 +0700

    Update SchemaGenerator.scala

--- app/utilities/SchemaGenerator.scala
+++ app/utilities/SchemaGenerator.scala
@@ -1,21 +1,7 @@
 package utilities
 
-import models.Category.CategoryTable
-import models.Customer.CustomerTable
-import models.ProductDetail.ProductDetailTable
-import models.Products.ProductsTable
-
 import scala.reflect.io.File
-import models.Role.RoleTable
-import models.Session.SessionTable
-import models.Shipment.ShipmentTable
-import models.Staff.StaffTable
-import models.Store.StoreTable
-import models.Supplier.SupplierTable
-import models.Transaction.TransactionTable
-import models.TransactionDetail.TransactionDetailTable
-import models.User.UserTable
-import models.UserProfile.UserProfileTable
+
 import slick.driver.PostgresDriver.api._
 
 object SchemaGenerator{
@@ -23,83 +9,70 @@ object SchemaGenerator{
   def run = {
     println("---------------------GENERATING SCHEMA.....")
 
-    val roles = TableQuery[RoleTable]
-    val categories = TableQuery[CategoryTable]
-    val users = TableQuery[UserTable]
-    val userProfile = TableQuery[UserProfileTable]
-    val sessions = TableQuery[SessionTable]
-    val staffs = TableQuery[StaffTable]
-    val customers = TableQuery[CustomerTable]
-    val products = TableQuery[ProductsTable]
-    val productDetail = TableQuery[ProductDetailTable]
-    val suppliers = TableQuery[SupplierTable]
-    val stores = TableQuery[StoreTable]
-    val transactions = TableQuery[TransactionTable]
-    val transactionDetail = TableQuery[TransactionDetailTable]
-    val shipments = TableQuery[ShipmentTable]
-
     val file = File("/Users/dedeandres/Documents/Skripsi_Project_BE/api-belanjayuk.id/belanjayukid-be-services/conf/evolutions/default/1.sql")
     val sb = new StringBuilder("# --- !Ups  \n\n")
-    roles.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.roleQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    categories.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.categoryQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    users.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.userQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    userProfile.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.userProfileQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    sessions.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.sessionsQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    staffs.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.staffQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    customers.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.customerQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    products.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.productQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    productDetail.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.productStockQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    suppliers.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.productDetailQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    stores.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.suppliersQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    transactions.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.storesQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    transactionDetail.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.transactionsQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    shipments.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.transactionDetailQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
+    sb.append("\n\n")
+    QueryUtility.shipmentsQuery.schema.create.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
 
     sb.append("\n\n")
     sb.append("# --- !Downs")
     sb.append("\n\n")
 
-    roles.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.roleQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    categories.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.categoryQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    users.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.userQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    userProfile.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.userProfileQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    sessions.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.sessionsQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    staffs.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.staffQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    customers.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.customerQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    products.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.productStockQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    productDetail.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.productDetailQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    suppliers.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.suppliersQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    stores.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.storesQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    transactions.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.transactionsQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    transactionDetail.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.transactionDetailQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     sb.append("\n\n")
-    shipments.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
+    QueryUtility.shipmentsQuery.schema.drop.statements.foreach(st => sb.append(st.toString + ";\n"))
     file.writeAll(sb.toString)
     play.Logger.info("----------------------FINISHED GENERATING SCHEMA--------------------------")
   }
