e031a88f9998be617eedb2817aaf6e08fbb63469
Dede Andres <dede.andres@dana.id>
2019-07-26 16:53:42 +0700

    Create ProductStockResolver.scala

---
+++ app/graphql/resolvers/ProductStockResolver.scala
@@ -0,0 +1,16 @@
+package graphql.resolvers
+
+import java.util.UUID
+
+import com.google.inject.Inject
+import models.ProductStock
+import repositories.repositoryInterfaces.ProductStockRepository
+import services.ProductStockService
+
+import scala.concurrent.Future
+
+class ProductStockResolver @Inject()(productStockService: ProductStockService){
+
+  def findProductStock(id: UUID): Future[Option[ProductStock]] = productStockService.findProductStock(id)
+
+}
