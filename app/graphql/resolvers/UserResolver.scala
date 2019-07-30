74b8f5df41b5c690309dc7ad57e0651a153ade62
Dede Andres <dede.andres@dana.id>
2019-07-30 13:34:22 +0700

    delete unused comment

--- app/graphql/resolvers/UserResolver.scala
+++ app/graphql/resolvers/UserResolver.scala
@@ -15,22 +15,10 @@ class UserResolver @Inject()(implicit val executionContext: ExecutionContext, us
 
   def users: Future[List[User]] = userService.users
 
-//  def addUser(user: User): Future[User] = userService.addUser(user)
-
   def deleteUser(id: UUID): Future[Boolean] = userService.deleteUser(id)
 
   def updateUser(id: UUID, username: String, password: String, email: String): Future[User] = userService.updateUser(id,username, password, email)
 
   def findUser(id: UUID): Future[Option[User]] = userService.findUser(id)
-
-  def login(username: String, password: String): Future[String] = userService.login(username, password)
-
-//  def inputUser(user: UserInput): Future[User] ={
-//    userService.addUser(user)
-//  }
-
-  def getUser(headers: Headers): Future[Option[User]] = {
-    userService.isAuthorized(headers)
-  }
 
 }
