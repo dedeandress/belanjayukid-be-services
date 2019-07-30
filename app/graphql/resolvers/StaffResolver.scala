b0d3525c4e530bb8ee739c6a47bf989b3cce610e
Dede Andres <dede.andres@dana.id>
2019-07-30 13:33:20 +0700

    Update StaffResolver.scala

--- app/graphql/resolvers/StaffResolver.scala
+++ app/graphql/resolvers/StaffResolver.scala
@@ -2,11 +2,14 @@ package graphql.resolvers
 
 import com.google.inject.Inject
 import graphql.input.StaffInput
-import models.Staff
-import services.StaffService
+import models.{LoginUser, Staff}
+import services.{StaffService, UserService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class StaffResolver @Inject()(staffService: StaffService, implicit val executionContext: ExecutionContext){
+class StaffResolver @Inject()(staffService: StaffService, userService: UserService, implicit val executionContext: ExecutionContext){
   def addStaff(staffInput: StaffInput): Future[Option[Staff]] = staffService.addStaff(staffInput)
+
+  def login(username: String, password: String): Future[LoginUser] = staffService.login(username, password)
+
 }
