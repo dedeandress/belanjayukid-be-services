225e440a31cbe4255dd30d4ec19e0ccdc6d0e6f0
Dede Andres <dede.andres@dana.id>
2019-08-01 17:24:04 +0700

    Update ProductResolver.scala
    add context

--- app/graphql/resolvers/ProductResolver.scala
+++ app/graphql/resolvers/ProductResolver.scala
@@ -3,6 +3,7 @@ package graphql.resolvers
 import java.util.UUID
 
 import com.google.inject.Inject
+import graphql.Context
 import graphql.input.ProductInput
 import models.Products
 import services.ProductService
@@ -13,6 +14,6 @@ class ProductResolver @Inject()(productService: ProductService){
 
   def product(id: UUID): Future[Option[Products]] = productService.findProduct(id)
 
-  def createProduct(productInput: ProductInput): Future[Products] = productService.addProduct(productInput)
+  def createProduct(context: Context, productInput: ProductInput): Future[Products] = productService.addProduct(context, productInput)
 
 }
