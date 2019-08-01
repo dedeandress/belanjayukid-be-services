6f62adb196c2693689f01f5225d8291dbf574db4
Dede Andres <dede.andres@dana.id>
2019-08-01 17:24:39 +0700

    Update ProductDetailRepositoryImpl.scala
    refactor code in logic to addProductDetail

--- app/repositories/ProductDetailRepositoryImpl.scala
+++ app/repositories/ProductDetailRepositoryImpl.scala
@@ -3,6 +3,7 @@ package repositories
 import java.util.UUID
 
 import com.google.inject.Inject
+import graphql.input.ProductInput
 import models.ProductDetail
 import modules.AppDatabase
 import repositories.repositoryInterfaces.ProductDetailRepository
@@ -17,22 +18,35 @@ class ProductDetailRepositoryImpl @Inject()(database: AppDatabase, implicit val 
 
   import profile.api._
 
-  override def findProductDetail(id: UUID): Future[Option[ProductDetail]] = db.run(Actions.findProductDetail(id))
+  override def findProductDetail(id: UUID): Future[Option[ProductDetail]] = {
+    play.Logger.warn("find ProductDetail")
+    db.run(Actions.findProductDetail(id))
+  }
 
-  override def addProductDetail(productDetail: ProductDetail): Future[ProductDetail] = db.run(Actions.addProductDetail(productDetail))
+  override def addProductDetail(productId: UUID, productInput: ProductInput): Future[Unit] = {
+    play.Logger.warn("add ProductDetail")
+    val insert = for(productDetail <- productInput.productDetailInput) yield {
+        val productDetailRow = new ProductDetail(
+          value = productDetail.value,
+          purchasePrice = productDetail.purchasePrice,
+          sellingPrice = productDetail.sellingPrice,
+          productId = productId,
+          productStockId = UUID.fromString(productDetail.productStockId))
+        QueryUtility.productDetailQuery += productDetailRow
+    }
+    db.run(DBIO.seq(insert: _*))
+  }
 
-  override def findProductDetailByProductId(productId: UUID): Future[Seq[ProductDetail]] = db.run(Actions.findProductDetailByProductId(productId))
+  override def findProductDetailByProductId(productId: UUID): Future[Seq[ProductDetail]] = {
+    play.Logger.warn("findProductDetailByProductId")
+    db.run(Actions.findProductDetailByProductId(productId))
+  }
 
   object Actions {
 
     def findProductDetail(id: UUID): DBIO[Option[ProductDetail]] = for{
       productDetail <- QueryUtility.productDetailQuery.filter(_.id === id).result.headOption
     }yield productDetail
-
-    def addProductDetail(productDetail: ProductDetail): DBIO[ProductDetail] = for{
-      id <- QueryUtility.productDetailQuery returning QueryUtility.productDetailQuery.map(_.id) += productDetail
-      productDetail <- findProductDetail(id)
-    }yield productDetail.get
 
     def findProductDetailByProductId(productId: UUID): DBIO[Seq[ProductDetail]] = for{
       productDetails <- QueryUtility.productDetailQuery.filter(_.productId === productId).result
