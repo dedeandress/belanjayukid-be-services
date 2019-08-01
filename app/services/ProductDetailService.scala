19fc5b8e5063044241e4ca58be5be348e279e393
Dede Andres <dede.andres@dana.id>
2019-08-01 17:24:55 +0700

    Update ProductDetailService.scala
    add context

--- app/services/ProductDetailService.scala
+++ app/services/ProductDetailService.scala
@@ -3,13 +3,18 @@ package services
 import java.util.UUID
 
 import com.google.inject.Inject
+import graphql.Context
 import models.ProductDetail
 import repositories.repositoryInterfaces.ProductDetailRepository
+import utilities.JWTUtility
 
 import scala.concurrent.Future
 
 class ProductDetailService @Inject()(productDetailRepository: ProductDetailRepository){
 
-  def findProductDetailByProductId(productId: UUID): Future[Seq[ProductDetail]] = productDetailRepository.findProductDetailByProductId(productId)
+  def findProductDetailByProductId(context: Context, productId: UUID): Future[Seq[ProductDetail]] = {
+    JWTUtility.isAdmin(context)
+    productDetailRepository.findProductDetailByProductId(productId)
+  }
 
 }
